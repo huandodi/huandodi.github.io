aa60d6fad4ac6303df9b17b9063bf34c624c94a4
huandodi <83649202+huandodi@users.noreply.github.com>
2023-04-02 15:42:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function typeWords(words, element) {
+  let index = 0;
+  const intervalId = setInterval(() => {
+    element.textContent += words[index];
+    index++;
+    if (index === words.length) {
+      clearInterval(intervalId);
+    }
+  }, 200);
+}
+
+
 const words = ["Linguistics", "Sociolinguistics", "Language Policy Planning", "Language Revitalisation"];
 const element = document.querySelector("#typing");
 
