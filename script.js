8dfddef1e708f4f28a8e09d5dffe2319e93140cc
huandodi <83649202+huandodi@users.noreply.github.com>
2023-04-02 16:15:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 function typeWords(words, element) {
-  let index = 0;
-  const intervalId = setInterval(() => {
-    element.textContent += words[index];
-    index++;
-    if (index === words.length) {
-      clearInterval(intervalId);
+  let wordIndex = 0;
+  let letterIndex = 0;
+  let direction = 1;
+  let intervalId = setInterval(() => {
+    const word = words[wordIndex];
+    const letters = word.slice(0, letterIndex + 1);
+    element.textContent = letters;
+    if (direction === 1) {
+      letterIndex++;
+      if (letterIndex === word.length) {
+        direction = -1;
+        setTimeout(() => {
+          direction = 1;
+          wordIndex++;
+          if (wordIndex === words.length) {
+            wordIndex = 0;
+          }
+        }, 1000);
+      }
+    } else {
+      letterIndex--;
+      if (letterIndex === -1) {
+        direction = 1;
+        wordIndex++;
+        if (wordIndex === words.length) {
+          wordIndex = 0;
+        }
+      }
     }
-  }, 100);
+  }, 75);
 }
-
-const words = ["Linguistics", "Sociolinguistics", "Language Policy Planning", "Language Revitalisation"];
-const element = document.getElementById("words");
+const words = ['Linguistics', 'Sociolinguistics', 'Language Policy Planning', 'Language Revitalisation'];
+const element = document.querySelector('#type-words');
 typeWords(words, element);
