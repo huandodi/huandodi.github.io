aad5c009a70aea99fd1784e72d799517bc3d3eff
huandodi <83649202+huandodi@users.noreply.github.com>
2023-04-02 15:41:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const words = ["Linguistics", "Sociolinguistics", "Language Policy Planning", "Language Revitalisation"];
+const element = document.querySelector("#typing");
+
+typeWords(words, element);
